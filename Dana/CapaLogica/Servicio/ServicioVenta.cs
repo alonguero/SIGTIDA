93b1fb1f94648a9a13ec9019c7127de17b39ced4
Eduardo Gamboa <eduargary93@gmail.com>
2019-11-05 11:51:41 -0600

    IngresarVenta

--- Dana/CapaLogica/Servicio/ServicioVenta.cs
+++ Dana/CapaLogica/Servicio/ServicioVenta.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dana.CapaConexion;
+using Dana.CapaLogica.LogicaNegocio;
+using MySql.Data.MySqlClient;
 
-namespace CapaLogica.Servicio
+namespace Dana.CapaLogica.Servicio
 {
-    class ServicioVenta
+    class ServicioVenta : servicio, IDisposable
     {
+        MySqlCommand miComando;
+        private string respuesta;
+
+
+
+        public ServicioVenta()
+        {
+            respuesta = "";
+            miComando = new MySqlCommand();
+        }
+        public void Dispose()
+        {
+
+        }
+
+        public string ingresarVenta(Detalle elDetalle)
+        {
+            miComando = new MySqlCommand();
+            Console.WriteLine("IngresarVenta");
+
+            miComando.CommandText = "IngresarVenta";
+
+            miComando.Parameters.Add("@idC", MySqlDbType.Int32);
+            miComando.Parameters["@idC"].Value = elDetalle.Id_Producto;
+
+           
+
+
+
+            respuesta = this.ejecutarSentencia(miComando);
+            if (respuesta == "")
+                respuesta += "se ha realizado correctamente la transaccion";
+
+
+            Console.WriteLine(respuesta);
+            Console.WriteLine("Fin del gestor ingreso Cliente");
+
+            return respuesta;
+        }
     }
 }
