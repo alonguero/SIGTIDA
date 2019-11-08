2317c095e61ef4703212119d5cf62e8ce6258af4
alonso <alonguero@>
2019-11-08 00:00:09 -0600

    Gestor (inventario, venta, producto)

--- Dana/CapaIntegracion/GestorInventario.cs
+++ Dana/CapaIntegracion/GestorInventario.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Dana.CapaConexion;
+using Dana.CapaLogica.LogicaNegocio;
+using Dana.CapaLogica.Servicio;
+using MySql.Data.MySqlClient;
 
-namespace CapaIntegracion
+namespace Dana.CapaIntegracion
 {
-    class GestorInventario
+    public class GestorInventario : servicio, IDisposable
     {
+
+        private string respuesta;
+        private MySqlCommand miComando;
+
+        public GestorInventario()
+        {
+            respuesta = "";
+            miComando = new MySqlCommand();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public string InsertarInventario(int cantidad_Producto)
+        {
+            Inventario nuevoInventario = new Inventario(cantidad_Producto);
+
+            using (ServicioInventario elInventario = new ServicioInventario())
+                return elInventario.ingresarInventario(nuevoInventario);
+        }
+
     }
 }
--- Dana/CapaIntegracion/GestorVentas.cs
+++ Dana/CapaIntegracion/GestorVentas.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Dana.CapaConexion;
+using Dana.CapaLogica.LogicaNegocio;
+using Dana.CapaLogica.Servicio;
+using MySql.Data.MySqlClient;
 
-namespace CapaIntegracion
+namespace Dana.CapaIntegracion
 {
-    class GestorVentas
+    public class GestorVentas: servicio, IDisposable
     {
+
+        private MySqlCommand miComando;
+        private string respuesta;
+
+        public GestorVentas()
+        {
+            respuesta = "";
+            miComando = new MySqlCommand();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public string InsertarVenta(int Id_Cliente, DateTime Fecha_Venta)
+        {
+            Venta nuevaVenta = new Venta(Id_Cliente, Fecha_Venta);
+
+            using (ServicioVenta laVenta = new ServicioVenta())
+                return laVenta.ingresarVenta(nuevaVenta);
+        }
+
+        public string modificarVenta(int Id_Venta, int Id_Cliente, DateTime Fecha_Venta)
+        {
+            Venta cambiaVenta = new Venta(Id_Venta, Id_Cliente, Fecha_Venta);
+
+            using (ServicioVenta laVenta = new ServicioVenta())
+                return laVenta.modificarVenta(cambiaVenta);
+        }
+
     }
 }
